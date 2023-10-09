770240ceebc64e11dbde1bccce369a8f1236a34a
JonatanLuna22 <jonatan.luna@student.forsbergsskola.se>
2023-10-09 14:22:54 +0200

    Create solution

---
+++ P1 Classes and Objects/Program.cs
@@ -0,0 +1,11 @@
+﻿/*
+Create a Console Project named P1People
+Create a Class named Person
+Create an array that contains 3 Person instances
+Print each Person to the Console using Console.WriteLine
+*/
+
+public class Person
+{
+    
+}
