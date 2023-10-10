df468c4f88b4a3511cef3c71185585cc30246f7a
JonatanLuna22 <jonatan.luna@student.forsbergsskola.se>
2023-10-10 15:54:59 +0200

    Create P4.1 Replacing

---
+++ P4.1 Replacing/Program.cs
@@ -0,0 +1,14 @@
+﻿/*
+Define a class named Plant
+Define a Field named isGrown which can store whether the Plant has grown into a tree.
+Define a Method PrintStatus which prints either "Plant is a seed." or "Plant is a tree." depending on its status.
+Define a Method Grow which prints "Plany is growing." and also changes the state of isGrown.
+Instantiate Plant and assign it to a variable named plant.
+Check the Status.
+Make it Grow.
+Check the Status.
+Make it Grow.
+Assign a new instance of class Plant to the same variable.
+Check the Status.
+*/
+
