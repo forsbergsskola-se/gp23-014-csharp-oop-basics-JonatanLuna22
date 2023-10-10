ba72cb659c7d8190d511e1498e4cc0be02ba4427
JonatanLuna22 <jonatan.luna@student.forsbergsskola.se>
2023-10-10 11:53:27 +0200

    P3 Level up

---
+++ P3 Level Up/GetExp.cs
@@ -0,0 +1,22 @@
+﻿
+public class GetExp {
+    public int Level;
+    public int Experience;
+    
+    public void GrantExperience() {
+        
+        Console.WriteLine("How many experience do you grant?");
+        int input = int.Parse(Console.ReadLine());
+        Experience = input;
+        
+        // Level Up
+        
+        while(Experience > 99) 
+        {
+            Level++;
+            Experience -= 100;
+        }
+        
+        Console.WriteLine($"Lvl: {Level}, Exp: {Experience}");
+    }
+}
---
+++ P3 Level Up/Program.cs
@@ -0,0 +1,15 @@
+﻿/*
+Create a Console Project named P3LevelUp
+Define a class named Player
+Define a Method named GrantExperience
+It takes an argument that defines how much Experience the player is supposed to get.
+It adds that experience to the Player's Experience.
+Every time, the Player has 100 Experience or more, he levels up.
+He keeps his extra experience.
+He can theoretically level up multiple Levels at once.
+Create an instance of class Player
+Allow the user repeatedly to define, how much Experience he wants to grant the Player and use the GrantExperience Method to grant it.
+*/
+
+GetExp getExp = new GetExp();
+getExp.GrantExperience();
