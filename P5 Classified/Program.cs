a17c69047bd83917307e7663e6430f81fd92e97b
JonatanLuna22 <jonatan.luna@student.forsbergsskola.se>
2023-10-12 12:08:23 +0200

    Create P5.2 Harvest

---
+++ P5 Classified/Program.cs
@@ -0,0 +1,55 @@
+﻿/*
+It's up to you to design classes and their inheritance.
+
+Create a Project named P5Classified
+Your Game has the following classes:
+
+Bread, Banana, Apple, Pear
+Sword, Axe, Pencil, Shield, Spear
+Tree, Gorilla, Chimpanzee, Sparrow, Pigeon, Hawk
+Design the classes by thinking about:
+
+Can they be held in your hands?
+Is it an object intended to be used in Combat?
+Do they live?
+Are they known to have many Vitamins?
+Can they be eaten?
+Hint: You need to add some additional classes. e.g.:
+
+If the given classes were Red, Blue, Green
+You might wanna add an additional class named Color
+Hint: Go a bit overboard! Every time you find two or more classes that have many traits in common, give them a common parent class, e.g.:
+
+If the given classes were Tulip, Dandelion, Oak, Fir
+You could add Flower as a parent for Tulip and Dandelion
+You could add Tree for Oak and Fir
+and Plant for Tree and Flower
+
+Bonus 1: Draw the Hierarchy as an Image using a Drawing Application or Miro
+
+Bonus 2: Use Mermaid to create a Mermaid Diagram for the Class Hierarchy and put it in your Repository's README.md
+*/
+
+/*
+Can they be held in your hands?
+Is it an object intended to be used in Combat?
+Do they live?
+Are they known to have many Vitamins?
+Can they be eaten?
+Hint: You need to add some additional classes. e.g.:
+*/
+
+/*
+Bread, Banana, Apple, Pear
+Sword, Axe, Pencil, Shield, Spear
+Tree, Gorilla, Chimpanzee, Sparrow, Pigeon, Hawk
+*/
+
+// public class item
+// public class food
+
+// public class weapon
+// public class livingBeing
+// public class food
+// public class vitamins
+ 
