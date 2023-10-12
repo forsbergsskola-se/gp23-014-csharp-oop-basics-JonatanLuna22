0bf2c3d9046b5b0a56c76d1a51b18978878251d1
JonatanLuna22 <jonatan.luna@student.forsbergsskola.se>
2023-10-12 16:12:09 +0200

    Create P4.2 All the same

---
+++ P4.2 All the same/Program.cs
@@ -0,0 +1,13 @@
+﻿/*
+Define a class named ScoreCounter
+It was a Field named Score for counting the Score
+It has a Method named IncreaseScore for increasing the Score
+Instantiate the class and assign it to Variable one
+Assign Variable one to Variable two
+Assign Variable two to Variable three
+Assign Variable three to Variable four
+Invoke IncreaseScore once on each Variable
+Print each Variable's Score to the Console
+Notice that they all have the Same Score of 4 (not one!)
+*/
+
