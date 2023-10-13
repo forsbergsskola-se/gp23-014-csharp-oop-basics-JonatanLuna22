544392eb4622d2ddb523ae5a2978b6075c26b843
JonatanLuna22 <jonatan.luna@student.forsbergsskola.se>
2023-10-13 17:09:51 +0200

    Complete P4.3 Passing references

---
+++ P4.3 Passing references/House.cs
@@ -0,0 +1,10 @@
+﻿
+public class House
+{
+    public string Owner = "Marc";
+    
+    public void PrintOwner()
+    {
+        Console.WriteLine($"This house is owner by {Owner}.");
+    }
+}
---
+++ P4.3 Passing references/Program.cs
@@ -0,0 +1,24 @@
+﻿/*
+Define a class named House
+It has a Field named Owner which stores the name of the Owner
+It has a Method named PrintOwner which prints "This house is owned by XX."
+In your Program.cs:
+Add a Function named Sell
+It takes an Argument of Type House
+It changes the Owner of that House to "Alex"
+Instantiate your House and assign it to a variable
+Invoke PrintOwner on the Variable
+Invoke Sell and pass the Variable as an Argument
+Invoke PrintOwner on the Variable
+*/
+
+House house1 = new House();
+
+void Sell(House house)
+{
+  house1.Owner = "Alex";
+}
+
+house1.PrintOwner();
+Sell(house1);
+house1.PrintOwner();
